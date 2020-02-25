1c366afde9652b543f129ce528ab7237f7367ca8
Aljosa Lazarevic Gostincar <lgaljo@gmail.com>
2020-02-25 18:49:33 +0100

    first repository

--- main.py
+++ main.py
@@ -229,9 +229,13 @@ def converter_post():
     enota = request.form.get("enota")
     stevilo = int(request.form.get("stevilo"))
     if str(enota) == "milliseconds":
-        answer = stevilo * 10*-3
-    else:
-        answer = "Hello world"
+        answer = stevilo * 10**-3
+    elif str(enota) == "microseconds":
+        answer = stevilo * 10**-6
+    elif str(enota) == "nanoseconds":
+        answer = stevilo * 10**-9
+    elif str(enota) == "picoseconds":
+        answer = stevilo * 10**-12
     return render_template("converter_rezultat.html", user=user, answer=answer)
 
 """""
